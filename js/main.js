20be9a3c7d63bddd2b4eda9afbeaaa889350b578
RareCodeBase <rarecodebasewebsite@gmail.com>
2025-04-14 09:55:19 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -109,3 +109,18 @@ function searchTutorials() {
     window.location.href = `/search.html?query=${encodeURIComponent(input)}`;
   }
 }
+
+
+const gtagScript = document.createElement("script");
+gtagScript.setAttribute("async", "");
+gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=AW-17002522934";
+document.head.appendChild(gtagScript);
+
+const gtagConfig = document.createElement("script");
+gtagConfig.innerHTML = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'AW-17002522934');
+`;
+document.head.appendChild(gtagConfig);
